cce9ee1662f512c7fa8bd760efa0b13ce7eb4f71
CedricSalaun <salaun.cedric@gmail.com>
2022-02-06 20:03:31 +0100

    PROJECT: change to VUE/NUXT

--- services/pages.js
+++
@@ -1,8 +0,0 @@
-export async function homeData() {
-  const res = await fetch('https://preview-galoo.000webhostapp.com/wp-json/');
-  return res.json();
-}
-export async function getPosts() {
-  const res = await fetch('https://preview-galoo.000webhostapp.com/wp-json/wp/v2/posts');
-  return res.json();
-}
