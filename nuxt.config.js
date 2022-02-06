f59c180fd5bd0f303a6bcaebd52e7b4cba69ce31
CedricSalaun <salaun.cedric@gmail.com>
2022-02-06 20:30:52 +0100

    docs: add meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,13 +2,15 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'preview_galoo_front',
-    htmlAttrs: {
-      lang: 'fr',
-    },
+    htmlAttrs: { lang: 'fr' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Le Fabuleux Book de Gaëlle Salaün.',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
